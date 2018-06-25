0024a2b9ec1b6e974b11e9b9d54592ae450e3a5f
alexkrv <aleksandr.krv@yandex.ru>
2018-06-25 22:09:56 +0300

    Input parameters check was added

--- src/answers/caesar_cipher.js
+++ src/answers/caesar_cipher.js
@@ -1,14 +1,17 @@
 function caesar_cypher( str, num ) {
+
     let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split(''),
         alphabetLngth = alphabet.length,
         input = str.split('')
 
+    num = num%alphabetLngth
+
     return input.map( el => {
-        var isLetter = alphabet.indexOf( el ) >= 0,
+        var isLetter = alphabet.indexOf( el.toLowerCase() ) >= 0,
             ind = -1,
             letterPosition = -1
         if( isLetter ) {
-            ind = alphabet.indexOf( el ) + num
+            ind = alphabet.indexOf( el.toLowerCase() ) + num
             letterPosition = ind >= 0 ? ind : alphabetLngth + ind
 
             return alphabet[ letterPosition%alphabetLngth ]
