c7150bbd61ada73387e40f91a5e1e81c115b2c30
alexkrv <aleksandr.krv@yandex.ru>
2018-07-05 23:46:04 +0300

    Work with `console` object was added

--- src/js_theory/async_await.js
+++ src/js_theory/async_await.js
@@ -2,6 +2,19 @@ async function simpleFunction() {
     let x1 = Number( await fetchData( 'http://localhost:3012/api/get_number' ) )
     let x2 = Number( await fetchData( 'http://localhost:3012/api/get_number' ) )
     console.log(`First = ${x1}\n\rSecond = ${x2}`)
+
+
+    //just for 'console.***' testing
+    console.trace()
+    console.log( console.memory )
+    for( let i = 0; i < 10; i++ ) {
+        console.count('This is count #')
+        console.assert( i < 5, "I is less than 5!")
+    }
+    console.log( "Memory %o. This is the %s-nd time of using console.memory object", console.memory, 2 )
+    console.table([{name:'first', age:'23'},{name:'second', age:'33'},{name:'third', age:'55'}])
+    //end of -- just for 'console.***' testing
+
     return x1 + x2
 }
 
