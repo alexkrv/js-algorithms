ea8f60f5115b26a49d8da648815e0c14bd104b08
alexkrv <aleksandr.krv@yandex.ru>
2018-10-03 00:10:09 +0300

    Fibonacci was refactored

--- src/answers/fibonacci.js
+++ src/answers/fibonacci.js
@@ -1,15 +1,7 @@
 function fibonacci( position ) {
-    let first = 1;
-    let second = 1;
-    let counter = 2;
-    return recursion( first, second, counter, position )
+    if( position < 3 )
+        return 1
+    else
+        return fibonacci(position - 1) + fibonacci( position -2)
 }
 
-function recursion( first, second, counter, position ) {
-    if ( counter === position )
-        return second
-
-    counter++
-    let result = first + second;
-    return recursion ( second, result, counter, position )
-}
