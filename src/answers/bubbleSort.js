18c9c890855aa8cdb45227495b215a36b70086b3
alexkrv <aleksandr.krv@yandex.ru>
2018-10-05 00:37:17 +0300

    Bubble sort improvement made

--- src/answers/bubbleSort.js
+++ src/answers/bubbleSort.js
@@ -1,11 +1,11 @@
 function bubbleSort( arr ) {
     let tmp = 0;
-    for( let j = 0; j < arr.length-1; j++ ) {
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] > arr[i + 1]) {
-                tmp = arr[i + 1];
-                arr[i + 1] = arr[i];
-                arr[i] = tmp;
+    for( let i = arr.length; i > 0; i-- ) {
+        for (let j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                tmp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = tmp;
             }
         }
     }
